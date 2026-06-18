c625fe1362c17920c53d6a56361ea7a9e23031a0
Aidan-Jared <AidanJared42@gmail.com>
2026-06-18 14:55:41 +1200

    adding in extra user

---
+++ modules/home/ajar667.nix
@@ -0,0 +1,29 @@
+{ self, inputs, ... }: {
+  flake.homeConfigurations.ajar667 = inputs.home-manager.lib.homeManagerConfiguration {
+        pkgs = import inputs.nixpkgs { system = "x86_64-linux"; };
+        modules = [
+          self.homeModules.ajar667Home
+          {
+            home.username = "ajar667";
+            home.homeDirectory = "ajar667";
+          }
+        ];
+  };
+
+  
+  flake.homeModules.samanthaHome = { pkgs, lib, ... }: {
+    imports = [
+      inputs.stylix.homeModules.stylix
+      self.homeModules.shellBash
+      self.homeModules.cliTools
+      self.homeModules.helix
+      self.homeModules.zellij
+      self.homeModules.starship
+      self.homeModules.atuin
+      self.homeModules.devenv
+    ];
+    home.stateVersion = "25.05";
+    programs.home-manager.enable = true;
+    # xdg.configFile."mimeapps.list".force = true;
+  };
+}
