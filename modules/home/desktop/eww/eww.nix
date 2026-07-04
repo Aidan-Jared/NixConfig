b2c3a21f945e6c320d3b0ccecf6f8a6ca518e872
Aidan-Jared <AidanJared42@gmail.com>
2026-07-04 17:41:41 +1200

    modifying eww

--- modules/home/desktop/eww/eww.nix
+++ modules/home/desktop/eww/eww.nix
@@ -12,8 +12,11 @@
     home.file = {
       ".config/eww/eww.yuck".source = ./eww.yuck;
       ".config/eww/eww.scss".source = ./eww.scss;
-      ".config/eww/niri-workspaces.sh".source = ./niri-workspaces.sh;
-      ".config/eww/sys-info.sh".source = ./sys-info.sh;
+      ".config/eww/Main".source = ./Main;
+      ".config/eww/Misc".source = ./Misc;
+      ".config/eww/Bar".source = ./Bar;
+      ".config/eww/Player".source = ./Player;
+      ".config/eww/System-Menu".source = ./System-Menu;
     };
 
     home.packages = [
